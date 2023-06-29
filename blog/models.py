867718243b8cf8c5b0bc7ee79aff44c796dbb08f
scottsmyth56 <>
2023-06-29 20:52:44 +0000

    Add User,Post,Comment,Like,Group Models

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,54 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
+from cloudinary_storage.storage import RawMediaCloudinaryStorage
 
-# Create your models here.
+
+class User(models.Model):
+    user_id = models.AutoField(primary_key=True)
+    first_name = models.CharField(max_length=150)
+    last_name = models.CharField(max_length=150)
+    username = models.CharField(max_length=150)
+    password = models.CharField(max_length=150)
+    profile_picture = CloudinaryField('image', default='placeholder')
+
+
+class Post(models.Model):
+    post_id = models.AutoField(primary_key=True)
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    image1 = CloudinaryField('image', default='placeholder')
+    image2 = CloudinaryField('image', default='placeholder')
+    timestamp = models.DateTimeField()
+    category = models.CharField(max_length=100, null=True, blank=True)
+    group = models.ForeignKey(Group, on_delete=models.CASCADE, null=True, blank=True)
+
+    class Meta:
+        ordering = ["-timestamp"]
+
+    def __str__(self):
+        return self.title
+
+
+class Comment(models.Model):
+    comment_id = models.AutoField(primary_key=True)
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    post_id = models.ForeignKey(Post, on_delete=models.CASCADE)
+    comment = models.TextField()
+    timestamp = models.DateTimeField()
+
+    class Meta:
+        ordering = ["-timestamp"]
+
+
+class Like(models.Model):
+    like_id - models.AutoField(primary_key=True)
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    post_id = models.ForeignKey(Post, on_delete=models.CASCADE)
+
+
+class Group(models.Model):
+    group_name = models.CharField(max_length=100)
+    description = models.TextField()
+    
