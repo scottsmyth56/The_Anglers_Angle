6841a85cfaa395f76fc53d55a0d84ac21d084b24
scottsmyth56 <>
2023-07-05 09:28:20 +0000

    Add card layout for posts on index.html

--- The_Anglers_Angle/urls.py
+++ The_Anglers_Angle/urls.py
@@ -16,10 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from blog import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('summernote/', include('django_summernote.urls')),
-    path("", include("blog.urls"), name="blog-urls"),
+    path("", include("blog.urls")),
 
 ]
