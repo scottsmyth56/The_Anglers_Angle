52bde110b5840eb8e2b382c5519d642d15f0bbf3
scottsmyth56 <>
2023-07-10 16:18:58 +0000

    Add view all Competitions

--- competitions/urls.py
+++ competitions/urls.py
@@ -2,7 +2,7 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    # path('competitiond', views.competitions.as_view(), name='competitions'),
+    path('competitions', views.viewCompetitions.as_view(), name='competitions'),
     path('add-competition/', views.addCompetition.as_view(), name='addCompetition'),
     # path('edit-post/<int:pk>', views.editPost.as_view(), name='editPost'),
     # path('delete-post/<int:pk>', views.deletePost.as_view(), name='deletePost'),
--- competitions/views.py
+++ competitions/views.py
@@ -5,7 +5,16 @@ from django.contrib import messages
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse_lazy
 
-# Create your views here.
+
+class viewCompetitions(generic.ListView, LoginRequiredMixin):
+    model = models.Competition
+    template_name = "Competitions/competitions.html"
+    context_object_name = 'competitions'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['user'] = self.request.user
+        return context
 
 
 class addCompetition(generic.CreateView):
@@ -13,7 +22,7 @@ class addCompetition(generic.CreateView):
     fields = ['title', 'location', 'description',
               'category', 'date', 'featuredImage']
     template_name = "Competitions/add_competition.html"
-    success_url = reverse_lazy('index')
+    success_url = reverse_lazy('competitions')
 
     def form_valid(self, form):
         form.instance.creator = self.request.user
