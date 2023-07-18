7abf4453c27f83d89b121f86ed3089a3a4056917
scottsmyth56 <>
2023-07-18 14:03:21 +0000

    Dummy Deploy Commit - Testing

--- The_Anglers_Angle/settings.py
+++ The_Anglers_Angle/settings.py
@@ -27,7 +27,8 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+X_FRAME_OPTIONS = 'SAMEORIGIN'
+DEBUG = False
 
 ALLOWED_HOSTS = ["8000-scottsmyth56-the-anglers-ahncinhraa.us2.codeanyapp.com",
                  'the-anglers-angle-b8d0becc6882.herokuapp.com', 'localhost']
