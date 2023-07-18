e48a7288966295d0c6827e9890b2007a13a73b39
Scott Smyth <smythscott60@gmail.com>
2023-07-18 21:34:47 +0100

    Update Styling for groups.html

--- blog/urls.py
+++ blog/urls.py
@@ -3,7 +3,6 @@ from django.urls import path
 
 urlpatterns = [
     path('post-lists', views.PostList.as_view(), name='post-list'),
-    # path('', views.index.as_view(), name='login'),
     path('index/', views.index.as_view(), name='index'),
     path('add-post/', views.addPost.as_view(), name='addPost'),
     path('edit-post/<int:pk>', views.editPost.as_view(), name='editPost'),
